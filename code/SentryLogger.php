1204264ef38a0d05c1466c07efcd215248b45e61
Stewart Wilson <stewartwilson@outlook.com>
2016-06-27 21:46:43 +1000

    Initial commit

---
+++ code/SentryLogger.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once 'Zend/Log/Writer/Abstract.php';
+
+/**
+ * Publish SilverStripe errors and warnings to Sentry using the Raven client.
+ */
+class SentryLogger extends Zend_Log_Writer_Abstract {
+
+    private $sentry;
+
+    private $logLevels = array(
+        'NOTICE'    => Raven_Client::INFO,
+        'WARN'      => Raven_Client::WARNING,
+        'ERR'       => Raven_Client::ERROR
+    );
+
+    /**
+     * @config
+     */
+    private static $sentry_dsn;
+
+
+    public static function factory($config) {
+        return new SentryLogger();
+    }
+
+    public function __construct()
+    {
+        $DSN = Config::inst()->get('SentryLogger', 'sentry_dsn');
+        $this->sentry = new Raven_Client($DSN);
+        $this->sentry->setEnvironment(Director::get_environment_type());
+    }
+
+    /**
+     * Send the error
+     */
+    public function _write($event) {
+        $data['level'] = $this->logLevels[$event['priorityName']];
+        $data['timestamp'] = $event['timestamp'];
+
+        $backtrace = SS_Backtrace::filter_backtrace(debug_backtrace(), array("SentryLogger->_write"));
+
+        $eventID = $this->sentry->captureMessage($event['message']['errstr'], array(), $data, $backtrace);
+    }
+
+}
